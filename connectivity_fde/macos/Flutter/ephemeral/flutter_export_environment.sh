630ea655ef77bf9cd681d1eb3546985bd8079405
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-10-16 19:48:38 +0300

    drop Flutter folder

--- connectivity_fde/macos/Flutter/ephemeral/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/r1zzu/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/r1zzu/w/turtle/flutter-desktop-embedding/plugins/example_plugin"
-export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_FRAMEWORK_DIR=/Users/r1zzu/flutter/bin/cache/artifacts/engine/darwin-x64"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
