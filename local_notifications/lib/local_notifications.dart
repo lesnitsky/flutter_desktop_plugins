aa00e6f966385142415e50ab24d24d759e6a06dd
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-10-16 16:09:42 +0300

    add docs; expose onClick and responses streams

--- local_notifications/lib/local_notifications.dart
+++ local_notifications/lib/local_notifications.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:local_notifications/src/notification_response.dart';
 import 'src/notification.dart';
 
 export 'src/notification.dart';
@@ -13,9 +14,20 @@ const _eventChannel = const EventChannel(
     'plugins.lesnitsky.com/local_notifications_event_channel');
 
 class LocalNotifications {
-  static bool _listening = false;
-  static Stream _eventsStream;
+  static Stream get _stream =>
+      _eventChannel.receiveBroadcastStream().map((d) => json.decode(d));
 
+  /// [Stream] of responses received from displayed notification.
+  /// Set [Notificaiton.requestReply] to true to show "Reply" button.
+  static Stream<NotificationResponse> get responses =>
+      _stream.where((d) => d['responseText'] != null).map((d) =>
+          NotificationResponse(id: d['id'], responseText: d['responseText']));
+
+  /// [Stream] of notification ids which where clicked.
+  static Stream<String> get onClick =>
+      _stream.where((d) => d['responseText'] == null).map((d) => d['id']);
+
+  /// Sends notification
   static send(Notification notification) async {
     try {
       final args = notification.toMap();
@@ -24,34 +36,12 @@ class LocalNotifications {
         'sendNotification',
         args,
       );
-
-      final responseCompleterCompleter = new Completer();
-      StreamSubscription sub;
-
-      if (notification.requestReply) {
-        if (!_listening) {
-          _eventsStream = _eventChannel.receiveBroadcastStream();
-          _listening = true;
-        }
-
-        sub = _eventsStream.map((d) => json.decode(d)).listen((data) {
-          if (data['id'] == notification.id) {
-            responseCompleterCompleter.complete(data['response']);
-            sub.cancel();
-          }
-        });
-      } else {
-        return null;
-      }
-
-      final responseCompleter = await responseCompleterCompleter.future;
-      sub.cancel();
-      return responseCompleter;
     } catch (err) {
       rethrow;
     }
   }
 
+  /// Overrides reply input placeholder text. Await returned future before sending the notification
   static Future<void> setReplyInputPlaceholderText(String text) async {
     try {
       await _methodChannel.invokeMethod(
---
+++ local_notifications/lib/src/notification_response.dart
@@ -0,0 +1,6 @@
+class NotificationResponse {
+  String id;
+  String responseText;
+
+  NotificationResponse({this.id, this.responseText});
+}
