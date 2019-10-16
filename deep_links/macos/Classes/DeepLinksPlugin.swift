b2a8cfda4d4abbda159b2900a56cfe8b44b0c2a2
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-10-16 19:52:44 +0300

    drop unnecessary imports and copyright comment

--- deep_links/macos/Classes/DeepLinksPlugin.swift
+++ deep_links/macos/Classes/DeepLinksPlugin.swift
@@ -1,22 +1,5 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import Cocoa
-import CoreWLAN
 import FlutterMacOS
-import Reachability
-import SystemConfiguration.CaptiveNetwork
 
 public class DeepLinksPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
   static let linkReceived = Notification.Name(rawValue: "linkReceived")
@@ -48,9 +31,10 @@ public class DeepLinksPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
 
     NotificationCenter.default.addObserver(
       self,
-      selector: #selector(self.onLinkReceived(_:)),
+      selector: #selector(onLinkReceived(_:)),
       name: DeepLinksPlugin.linkReceived,
-      object: nil)
+      object: nil
+    )
 
     return nil
   }
