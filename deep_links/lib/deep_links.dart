d4f2b98f00f6858d8dc87dde93fb6dc8bde2e501
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2021-08-27 23:55:21 +0300

    migrate deep links to null-safety

--- deep_links/lib/deep_links.dart
+++ deep_links/lib/deep_links.dart
@@ -3,13 +3,14 @@ import 'package:flutter/services.dart';
 const _eventChannel = const EventChannel('plugins.lesnitsky.com/deep_links');
 
 class DeepLinks {
-  Stream<String> _onLinkReceived;
+  Stream<String>? _onLinkReceived;
 
   Stream<String> get onLinkReceived {
-    if (_onLinkReceived != null) return _onLinkReceived;
+    if (_onLinkReceived != null) return _onLinkReceived!;
     _onLinkReceived = _eventChannel
         .receiveBroadcastStream()
         .map((dynamic event) => event as String);
-    return _onLinkReceived;
+
+    return _onLinkReceived!;
   }
 }
